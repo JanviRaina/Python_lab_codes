063a9b054722036d65aa68009774ddb503b28b15
JanviRaina <50794649+JanviRaina@users.noreply.github.com>
2020-05-01 19:36:11 +0530

    Updated Exp 7.

--- Experiment7.py
+++ Experiment7.py
@@ -1,3 +1,15 @@
+"""
+Given a non negative integer A, print all the pairs of integers(a,b) such that
+a and b are positive integers
+a<=b and
+a^2 + b^2 = A
+0 <= A
+A<=1000
+
+Problem statement is pretty clear, We just need to find such pairs who satisfies the Pythagoras theorem up-to that number.
+The idea is to put two nested loops and print those numbers whose squared sum is equal to the given number.
+"""
+
 n=int(input("enter a number"))
 for i in range(1,n):
     for j in range(1,n):
